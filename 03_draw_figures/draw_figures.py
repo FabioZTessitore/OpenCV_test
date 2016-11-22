85a09d3f1b5b6f4e8de2809cf200f75caeb20340
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-11-22 13:26:44 +0100

    text

--- 03_draw_figures/draw_figures.py
+++ 03_draw_figures/draw_figures.py
@@ -8,8 +8,10 @@ img = np.zeros((512, 512, 3), np.int8)
 
 cv2.line(img, (0, 0), (511, 511), (255, 0, 0), 5)
 cv2.rectangle(img, (50, 0), (200, 190), (255, 255, 0), 3)
-cv2.circle(img, (100, 100), 40, (0, 255, 255), 3, cv2.LINE_AA)
+cv2.circle(img, (100, 100), 40, (0, 255, 255), 3)
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img, 'Hello', (10, 500), font, 4, (255, 255, 255), 2)
 while True:
     cv2.imshow('image', img)
 
